7f6980a4182c338b097cb7c6483dbd1750e1b3b6
Gonzalo <gonzalo.trucco40@gmail.com>
2021-06-28 16:14:29 -0300

    Rock Paper Scissor

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ let UserScore = 0;
 let result;
 
 const buttons = document.querySelectorAll('button');
-// we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
-
-  // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
         if (ComputerScore === 5 || UserScore === 5) {
             return;
@@ -18,12 +15,13 @@ buttons.forEach((button) => {
         UserSelection = button.id;
         ComputerSelection = ComputerPlay();
         Round(ComputerSelection, UserSelection);
+        deleteClass();
+        displayImage();
   });
 });
 
 
 const newGame = document.querySelector('.refresh');
-// Creating a button to refresh page
 newGame.addEventListener('click', refreshPage);
 newGame.classList.add('hide');
 
@@ -34,8 +32,8 @@ function ComputerPlay() {
 
 function Round(ComputerSelection, UserSelection) {
 
-const comments = document.getElementById('comments');
-comments.textContent = "";
+    const comments = document.getElementById('comments');
+    comments.textContent = "";
 
 	if (ComputerSelection === UserSelection) { 
       comments.textContent = "Its a tie";
@@ -65,15 +63,14 @@ comments.textContent = "";
         comments.textContent = "Rock beats Scissors! Good job!";
 		}
     
-const scoreNumberComputer = document.getElementById('ComputerScore');
-const scoreNumberUser = document.getElementById('UserScore');
-const header = document.getElementById('header');
-const headerWinner = document.getElementById('headerWinner');
-scoreNumberComputer.textContent = ComputerScore;
-scoreNumberUser.textContent = UserScore;
-
-header.textContent = "";
-headerWinner.textContent = "";
+    const scoreNumberComputer = document.getElementById('ComputerScore');
+    const scoreNumberUser = document.getElementById('UserScore');
+    const header = document.getElementById('header');
+    const headerWinner = document.getElementById('headerWinner');
+    scoreNumberComputer.textContent = ComputerScore;
+    scoreNumberUser.textContent = UserScore;
+    header.textContent = "";
+    headerWinner.textContent = "";
       
     if (ComputerScore === 5 || UserScore === 5) {
 		header.textContent = "WE HAVE A WINNER!!";
@@ -88,7 +85,52 @@ headerWinner.textContent = "";
 
 function refreshPage(){
 
+    const scoreNumberComputer = document.getElementById('ComputerScore');
+    const scoreNumberUser = document.getElementById('UserScore');
+
     ComputerScore = 0;
     UserScore = 0;
+    newGame.classList.remove('show');
     newGame.classList.add('hide');
+    comments.textContent = "";
+    header.textContent = "";
+    headerWinner.textContent = "";
+    scoreNumberComputer.textContent = 0;
+    scoreNumberUser.textContent = 0;
+    deleteClass();
+}
+
+function displayImage(){
+    const computerDisplay = document.getElementById('computerDisplay');
+    const userDisplay = document.getElementById('userDisplay');
+
+    if (item == 'rock') {
+        computerDisplay.classList.add('rock-image');
+    } else if (item == 'paper') {
+        computerDisplay.classList.add('hand-image');
+    } else {
+        computerDisplay.classList.add('scissor-image');
+    }
+
+    if(UserSelection == 'rock'){
+        userDisplay.classList.add('rock-image');
+    } else if (UserSelection == 'paper') {
+        userDisplay.classList.add('hand-image');
+    } else {
+        userDisplay.classList.add('scissor-image');
+    }
+}
+
+function deleteClass(){
+    const computerDisplay = document.getElementById('computerDisplay');
+    const userDisplay = document.getElementById('userDisplay');
+
+    computerDisplay.classList.remove('rock-image');
+    computerDisplay.classList.remove('hand-image');
+    computerDisplay.classList.remove('scissor-image');
+
+    userDisplay.classList.remove('rock-image');
+    userDisplay.classList.remove('hand-image');
+    userDisplay.classList.remove('scissor-image');
+
 }
